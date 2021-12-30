b6fc5375dd8b49bf7fe93405943859d54059c54c
liupeixin <fengyi.mail@gmail.com>
2021-12-30 22:38:08 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  extends: ["standard", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: "avoid",
+  singleQuote: false,
+  quoteProps: "preserve",
+  trailingComma: "none",
+  bracketSpacing: true,
+  semi: true,
+  vueIndentScriptAndStyle: false,
+  bracketSameLine: true,
+  trailingComma: "es5",
+};
